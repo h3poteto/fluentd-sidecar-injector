6bc75c37c2d4f7629e899fde9a7afbc8fd6dd63a
AkiraFukushima <h3.poteto@gmail.com>
2020-12-30 00:25:46 +0900

    Apply and cleanup CRD and RBAC around e2e tests

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -1,12 +1,63 @@
 package e2e_test
 
 import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/h3poteto/fluentd-sidecar-injector/e2e/pkg/util"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"github.com/h3poteto/fluentd-sidecar-injector/e2e"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
-var _ = Describe("E2e", func() {
+var _ = Describe("E2E", func() {
+	BeforeSuite(func() {
+		// Deploy operator controller
+		configfile := os.Getenv("KUBECONFIG")
+		if configfile == "" {
+			configfile = "$HOME/.kube/config"
+		}
+		restConfig, err := clientcmd.BuildConfigFromFlags("", os.ExpandEnv(configfile))
+		if err != nil {
+			panic(err)
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+		if err := util.ApplyCRD(ctx, restConfig); err != nil {
+			panic(err)
+		}
+		if err := util.ApplyRBAC(ctx, restConfig); err != nil {
+			panic(err)
+		}
 
+	})
+	AfterSuite(func() {
+		// Delete operator controller and custom resources
+		configfile := os.Getenv("KUBECONFIG")
+		if configfile == "" {
+			configfile = "$HOME/.kube/config"
+		}
+		restConfig, err := clientcmd.BuildConfigFromFlags("", os.ExpandEnv(configfile))
+		if err != nil {
+			panic(err)
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+		if err := util.DeleteCRD(ctx, restConfig); err != nil {
+			panic(err)
+		}
+		if err := util.DeleteRBAC(ctx, restConfig); err != nil {
+			panic(err)
+		}
+	})
+	Describe("Operator", func() {
+		// Check deploying custom resources
+		It("Sample", func() {
+			Expect(true).To(Equal(true))
+		})
+	})
+	Describe("Webhook", func() {
+		// Check webhook are injected
+	})
 })
--- go.mod
+++ go.mod
@@ -19,4 +19,5 @@ require (
 	k8s.io/klog/v2 v2.4.0
 	k8s.io/utils v0.0.0-20200729134348-d5654de09c73
 	sigs.k8s.io/structured-merge-diff/v3 v3.0.0 // indirect
+	sigs.k8s.io/yaml v1.2.0
 )
